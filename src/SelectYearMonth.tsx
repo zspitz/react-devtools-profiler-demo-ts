b18bdc788cee682f62a443bf30ed88c02a09f939
Zev Spitz <shivisi@hotmail.com>
2024-04-07 07:23:04 +0300

    fix

--- src/SelectYearMonth.tsx
+++ src/SelectYearMonth.tsx
@@ -6,6 +6,8 @@ type Props = {
     onChange: (e: ChangeEvent<HTMLSelectElement>) => void
 }
 
+const monthNames = range(12).map(i => new Date(0, 11 - i).toLocaleString('he-IL', { month: 'long' }))
+
 const SelectYearMonth = ({ value, onChange }: Props) => {
     const currentYear = new Date().getFullYear()
 
@@ -13,10 +15,9 @@ const SelectYearMonth = ({ value, onChange }: Props) => {
         <select onChange={onChange} value={value}>
             {range(currentYear - 1899, currentYear, -1).map(year => (
                 <optgroup key={year} label={`${year}`}>
-                    {range(12).map(month => {
-                        const monthName = new Date(0, 11 - month).toLocaleString('he-IL', { month: 'long' })
+                    {monthNames.map((monthName, month) => {
                         return (
-                            <option key={month} value={`${year}-${month + 1}`}>
+                            <option key={monthName} value={`${year}-${month + 1}`}>
                                 {monthName} - {year}
                             </option>
                         )
