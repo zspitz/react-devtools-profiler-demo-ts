fdbc8b8680ea7d1a36bce2b6fdf49cddee51f53c
Zev Spitz <shivisi@hotmail.com>
2024-11-25 17:32:36 +0200

    Update/fix

--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,1 @@
-export const range = (length: number, start = 0, step = 1) => [...Array(length)].map((_, i) => i * step + start);
+export const range = (length: number, start = 0, step = 1) => [...Array<undefined>(length)].map((_, i) => i * step + start)
